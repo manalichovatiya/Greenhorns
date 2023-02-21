93ea3abf7568f5337e3baf30af612e0b2a6fc86f
manalichovatiya <115995019+manalichovatiya@users.noreply.github.com>
2023-02-21 12:13:11 +0530

    Add files via upload

---
+++ Greenhorns-2(1).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main(){
+	 int a = 78912;
+	 int b = 12724612378;
+	 a=a+b;
+	 b=a-b;
+	 a=a-b;
+		 printf("a=%d",a);
+		 printf("b=%d",b);
+	 
+}
---
+++ Greenhorns-2(2).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main(){
+	 int a = 3679;
+	 int b = 18123;
+	 int c;
+	 c=a;
+	 a=b;
+	 b=c;
+	 printf("a=%d",a);
+	 printf("b=%d",b);
+	 
+}
